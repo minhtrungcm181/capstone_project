54146b6282e717d4c1db6c3d233844931495e274
minhtrungcm181 <minhtrungcm181@gmail.com>
2023-10-30 23:04:58 +0700

    api upload

--- src/components/UploadFilmForm.jsx
+++ src/components/UploadFilmForm.jsx
@@ -1,46 +1,67 @@
+import axios from "axios";
 import React from "react"
 class UploadFilmForm extends React.Component {
-    options = {
-        method: 'POST',
-        headers: {
-            Accept: 'application/form-data',
-            'content-type': 'multipart/form-data',
-        },
-        body: this.state
-    };
-    handleChangeMovieInfo = (event) => {
+    filemp4 = null;
+    fileJpg = null;
+    formData = new FormData();
+    
+    handleChangeMovieTitle = (event) => {
         this.setState({
-            movieTitle: event.target.value,
-            movieCurrentEp: event.target.value,
-            movieTotalE: event.target.value,
+            movieTitle: event.target.value
         })
     }
-    handleChangeMovieSource = (event) => {
-        const filemp4 = event.target.files[0]
+    handleChangeMovieCurrentEp = (event) => {
         this.setState({
-            filemp4: filemp4
+            movieCurrentEp: event.target.value
         })
     }
-    handleChangeMovieJpg = (event) => {
-        const filejpg = event.target.files[1]
+    handleChangeMovieTotalEp = (event) => {
         this.setState({
-            filejpg: filejpg
+            movieTotalEp: event.target.value
         })
+    }
+    // handleChangeMovieSource = (event) => {
+    //     const filemp4 = event.target.files[0]
+    //     this.setState({
+    //         filemp4: filemp4
+    //     })
+    // }
+    // handleChangeMovieJpg = (event) => {
+    //     const filejpg = event.target.files[1]
+    //     this.setState({
+    //         filejpg: filejpg
+    //     })
+    // }
+    handleChangeFileSubmit = (event) => {
+        for (const item of event.target.files) {
+            if (item.type === "video/mp4") {
+                this.filemp4 = item
+            }
+            else if (item.type === "image/jpeg") {
+                this.fileJpg = item
+            }
+
+        }
     }
     handleSubmit = (event) => {
-        // fetch('http://localhost:5000/film/upload', this.options)
-        //     .then(response => response.json())
-        //     .then(data => console.log(data))
-        //     .catch(error => console.error(error));
-        // console.log(this.options.body)
-        console.log(this.state);
+        this.formData.append("movieTitle", this.state.movieTitle);
+        this.formData.append("movieCurrentEp", this.state.movieCurrentEp);
+        this.formData.append("movieTotalEp", this.state.movieTotalEp);
+        this.formData.append("filemp4", this.filemp4);
+        this.formData.append("fileJpg", this.fileJpg);
+        axios({
+            method: 'post',
+            url: 'http://localhost:5000/film/upload',
+            data: this.formData
+        }).catch(error => console.log(error));
+        console.log(this.formData)
+
     }
     state = {
         movieTitle: "",
-        movieCurrentEp: "",
-        movieTotalEp: "",
-        filemp4:[],
-        filejpg:[]
+        movieCurrentEp: null,
+        movieTotalEp: null,
+        files: [],
     }
 
     render() {
@@ -52,37 +73,37 @@ class UploadFilmForm extends React.Component {
                         id="movie title"
                         name="movieTitle"
                         value={this.movieTitle}
-                        onChange={(event) => this.handleChangeMovieInfo(event)}
+                        onChange={(event) => this.handleChangeMovieTitle(event)}
                     />
-                    <label for="movie current ep">Movie name:</label>
-                    <input type="text"
+                    <label for="movie current ep">Movie Current Ep:</label>
+                    <input type="number"
                         id="movie current ep"
                         name="movieCurrentEp"
                         value={this.movieCurrentEp}
-                        onChange={(event) => this.handleChangeMovieInfo(event)}
+                        onChange={(event) => this.handleChangeMovieCurrentEp(event)}
                     />
-                    <label for="movie total ep">Movie name:</label>
-                    <input type="text"
+                    <label for="movie total ep">Movie Total Ep:</label>
+                    <input type="number"
                         id="movie total ep"
                         name="movieTotalEp"
                         value={this.movieTotalEp}
-                        onChange={(event) => this.handleChangeMovieInfo(event)}
+                        onChange={(event) => this.handleChangeMovieTotalEp(event)}
                     />
 
                     <label for="Source Film">Source Film:</label>
                     <input type="file"
                         id="source film"
                         name="filemp4"
-                        value={this.filemp4}
-                        onChange={(event) => this.handleChangeMovieSource(event)}
+                        value={this.files}
+                        onChange={(event) => this.handleChangeFileSubmit(event)}
                     />
 
                     <label for="Source Jpg">Source Jpg:</label>
                     <input type="file"
                         id="source film"
                         name="filejpg"
-                        value={this.filejpg}
-                        onChange={(event) => this.handleChangeMovieJpg(event)}
+                        value={this.files}
+                        onChange={(event) => this.handleChangeFileSubmit(event)}
                     />
                     <input type="button" value="upload"
                         onClick={(event) => this.handleSubmit(event)}
