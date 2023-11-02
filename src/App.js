b249653cf139e760e6b3699da2e67aa2fc86c72f
minhtrungcm181 <minhtrungcm181@gmail.com>
2023-11-02 15:30:41 +0700

    sidebar and topbar

--- src/App.js
+++ src/App.js
@@ -2,23 +2,27 @@ import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import Topbar from "./views/global/Topbar";
 import Sidebar from "./views/global/Sidebar";
-
+import { Routes, Route } from "react-router-dom";
+import MovieManage from "./views/moviemanage/MovieManage";
+import { useState } from "react";
 function App() {
   const [theme, colorMode] = useMode()
+  const [isSidebar, setIsSidebar] = useState(true);
 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className='app'>
+          <Sidebar isSidebar={isSidebar} />
           <main className="content">
-            <Topbar />
-            {/* <Routes>
-              <Route></Route>
-              <Route></Route>
-              <Route></Route>
-              <Route></Route>
-            </Routes> */}
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path ="/moviemanage" element={<MovieManage/>}/>
+              {/* <Route path ="" element=""/>
+              <Route path ="" element=""/>
+              <Route path ="" element=""/> */}
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
---
+++ src/views/moviemanage/MovieManage.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+class MovieManage extends React.Component {
+    handleClick = () => {
+        alert('ok')
+    }
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+export default MovieManage;
